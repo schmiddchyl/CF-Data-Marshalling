2108bd7d55fad562eb71885dcc4898e817ee933b
Daniel Schmidt <Daniel.Schmidt@hyland.com>
2024-10-03 15:23:40 -0400

    initial commit

---
+++ CF-Data-Marshalling/CF-Data-Marshalling/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CF_Data_Marshalling
+{
+    public class Class1
+    {
+    }
+}
---
+++ CF-Data-Marshalling/CF-Data-Marshalling/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CF-Data-Marshalling")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hyland Software, Inc.")]
+[assembly: AssemblyProduct("CF-Data-Marshalling")]
+[assembly: AssemblyCopyright("Copyright © Hyland Software, Inc. 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aeddfe7a-4495-4b30-bccd-5c90f0df5e6f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
